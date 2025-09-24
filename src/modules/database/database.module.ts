d9de1de57050ce5449de2c4f9176f3a87c898993
MakhmudSD <makhwork15@gmail.com>
2025-09-24 23:20:39 +0900

    fix: env removed

--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -18,11 +18,11 @@ import { Order } from '../orders/order.entity';
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get<string>('DB_HOST', 'localhost'),
-        port: configService.get<number>('DB_PORT', 5432),
-        username: configService.get<string>('DB_USERNAME', 'postgres'),
-        password: configService.get<string>('DB_PASSWORD', 'Mahmud2001'),
-        database: configService.get<string>('DB_DATABASE', 'postgres'),
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME',),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
         entities: [User, Role, Driver, Order],
         synchronize: false, // Never modify existing schema
         logging: false, // Disable query logging for production
